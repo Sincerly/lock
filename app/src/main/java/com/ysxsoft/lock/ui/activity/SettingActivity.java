29a052b7dc9a540d259f278fb53c3d9f7886883a
huguangcai <2694675654@qq.com>
2019-12-20 11:13:47 +0800

    提交 设置界面的跳转

--- app/src/main/java/com/ysxsoft/lock/ui/activity/SettingActivity.java
+++ app/src/main/java/com/ysxsoft/lock/ui/activity/SettingActivity.java
@@ -1,5 +1,8 @@
 package com.ysxsoft.lock.ui.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,10 +11,15 @@ import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.bumptech.glide.Glide;
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.ysxsoft.common_base.base.BaseActivity;
+import com.ysxsoft.common_base.utils.ImageUtils;
 import com.ysxsoft.common_base.utils.JsonUtils;
 import com.ysxsoft.common_base.utils.SharedPreferencesUtils;
 import com.ysxsoft.common_base.view.custom.image.CircleImageView;
+import com.ysxsoft.lock.config.AppConfig;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
@@ -20,9 +28,17 @@ import com.ysxsoft.lock.R;
 import com.ysxsoft.lock.models.response.SettingResponse;
 import com.ysxsoft.lock.net.Api;
 
+import java.io.File;
+import java.util.List;
+
+import androidx.annotation.Nullable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPickerActivity;
+import cn.bingoogolapple.photopicker.util.BGAPhotoHelper;
+import cn.bingoogolapple.photopicker.util.BGAPhotoPickerUtil;
+import io.reactivex.functions.Consumer;
 import okhttp3.Call;
 
 /**
@@ -75,7 +91,10 @@ public class SettingActivity extends BaseActivity {
     LinearLayout LL5;
     @BindView(R.id.LL6)
     LinearLayout LL6;
-
+    private BGAPhotoHelper mPhotoHelper;
+    private RxPermissions r;
+    private static final int RC_CHOOSE_PHOTO = 0x01;
+    public static final int REQUEST_CODE_CROP = 0x02;
 
     public static void start() {
         ARouter.getInstance().build(ARouterPath.getSettingActivity()).navigation();
@@ -90,6 +109,25 @@ public class SettingActivity extends BaseActivity {
     public void doWork() {
         super.doWork();
         initTitle();
+        initPhotoHelper();
+    }
+
+    private void initPhotoHelper() {
+        r = new RxPermissions(this);
+        if (r.isGranted(Manifest.permission.READ_EXTERNAL_STORAGE) && r.isGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            File takePhotoDir = new File(AppConfig.PHOTO_CACHE_PATH);
+            mPhotoHelper = new BGAPhotoHelper(takePhotoDir);
+        } else {
+            r.requestEach(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}).subscribe(new Consumer<Permission>() {
+                @Override
+                public void accept(Permission permission) throws Exception {
+                    if (permission.granted) {
+                        File takePhotoDir = new File(AppConfig.PHOTO_CACHE_PATH);
+                        mPhotoHelper = new BGAPhotoHelper(takePhotoDir);
+                    }
+                }
+            });
+        }
     }
 
     private void initTitle() {
@@ -106,6 +144,7 @@ public class SettingActivity extends BaseActivity {
                 backToActivity();
                 break;
             case R.id.LL1:
+                choicePhotoWrapper();
                 break;
             case R.id.LL2:
                 EditInfoActivity.start();
@@ -113,15 +152,40 @@ public class SettingActivity extends BaseActivity {
             case R.id.LL3:
                 break;
             case R.id.LL4:
+                ForgetPwdActivity.start();
                 break;
             case R.id.LL5:
                 break;
             case R.id.LL6:
+                AboutMeActivity.start();
                 break;
             case R.id.LoginOut:
                 toLogin();
                 break;
         }
+    }
+
+    private void choicePhotoWrapper() {
+        r.request(Manifest.permission.CAMERA).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                if (aBoolean) {
+                    // 拍照后照片的存放目录，改成你自己拍照后要存放照片的目录。如果不传递该参数的话就没有拍照功能
+                    File takePhotoDir = new File(AppConfig.PHOTO_PATH, "space");
+                    File f = new File(AppConfig.PHOTO_PATH);
+                    if (!f.exists()) {
+                        f.mkdirs();
+                    }
+                    Intent photoPickerIntent = new BGAPhotoPickerActivity.IntentBuilder(SettingActivity.this)
+                            .cameraFileDir(takePhotoDir) // 拍照后照片的存放目录，改成你自己拍照后要存放照片的目录。如果不传递该参数的话则不开启图库里的拍照功能
+                            .maxChooseCount(1) // 图片选择张数的最大值
+                            .selectedPhotos(null) // 当前已选中的图片路径集合
+                            .pauseOnScroll(false) // 滚动列表时是否暂停加载图片
+                            .build();
+                    startActivityForResult(photoPickerIntent, RC_CHOOSE_PHOTO);
+                }
+            }
+        });
     }
 
     public void request() {
@@ -155,5 +219,40 @@ public class SettingActivity extends BaseActivity {
                         }
                     }
                 });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case RC_CHOOSE_PHOTO:
+                    if (data != null) {
+                        List<String> selectedPhotos = BGAPhotoPickerActivity.getSelectedPhotos(data);
+                        if (selectedPhotos != null && selectedPhotos.size() > 0) {
+                            //拍照返回
+                            try {
+                                startActivityForResult(mPhotoHelper.getCropIntent(selectedPhotos.get(0), 200, 200), REQUEST_CODE_CROP);
+                            } catch (Exception e) {
+                                mPhotoHelper.deleteCameraFile();
+                                mPhotoHelper.deleteCropFile();
+                                BGAPhotoPickerUtil.show(R.string.bga_pp_not_support_crop);
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    break;
+                case REQUEST_CODE_CROP:
+                    String cropPath = mPhotoHelper.getCropFilePath();
+                    String path = ImageUtils.compress(this, System.currentTimeMillis() + "", new File(cropPath), AppConfig.PHOTO_PATH);
+                    //裁剪后的
+                    Glide.with(mContext).load(new File(path)).into(ivAvatar);
+
+                    //edit(null, null, path, null, null);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
